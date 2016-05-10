cccd466ad61dc9b7425d7a933f41fc3c6260db57
tejasgarde <tejasgarde@yahoo.co.in>
2016-05-11 01:27:25 +0530

    Simple Insert and Retrive

--- src/main/java/org/hibernate/init/HibernateInitializer.java
+++ src/main/java/org/hibernate/init/HibernateInitializer.java
@@ -10,25 +10,58 @@ import org.hibernate.dao.UserDetails;
 
 public class HibernateInitializer {
 	
+	private static SessionFactory factory ;
+	
 	public static void main(String[] args) {
+		
+		HibernateInitializer.simpleInsert();
+	
+		HibernateInitializer.simpleRetriveUserDetails();
+	}
+	
+	
+	private static void simpleInsert(){
 		UserDetails user = new UserDetails();
 		
 		user.setUserId(1);
 		user.setUserName("First user");
 		
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		factory = HibernateInitializer.getSessionFactory();
 		
 		Session session = factory.openSession();
 		
 		Transaction trans = session.beginTransaction();
-		
+		// Insert Data 
 		session.save(user);
 		trans.commit();
+		session.close();
+	}
+	
+	
+	private  static void simpleRetriveUserDetails(){
+		UserDetails user ;
+		factory = HibernateInitializer.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction trans = session.beginTransaction();
 		
-		
-		
+		user = (UserDetails) session.get(UserDetails.class, 1);
+		if(user != null){
+			System.out.println("User Name is : "+user.getUserName());
+			System.out.println("User ID is : "+user.getUserId());
+		}else{
+			System.out.println("User Not Found");
+		}
 		
 		
 	}
+	
+	private static  SessionFactory getSessionFactory(){
+		if(factory == null  ){
+			return new Configuration().configure().buildSessionFactory();
+		}
+		return factory;
+	}
+	
+	
 
 }
